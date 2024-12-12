3898bb636b52acd56af0efb9192162a96178508b
ChristianRekfild <ChrisRekfild@gmail.com>
2024-12-13 00:00:16 +0500

    Создана инфраструктура под основной проект

--- Views/Windows/MainWindow.xaml.cs
+++ Views/Windows/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LearnWPF
+namespace LearnWPF.Views.Windows
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
