b9a3cf958c136d9da478da84ab65fe9badbd58ff
ChristianRekfild <ChrisRekfild@gmail.com>
2024-12-12 23:34:58 +0500

    Создана базовая инфраструктура

---
+++ Tests/LearnWPF_Tests/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LearnWPF_Tests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
