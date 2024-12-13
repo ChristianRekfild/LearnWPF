feb210aa6a54c53e9432e205c17d5a04ba118f3c
ChristianRekfild <ChrisRekfild@gmail.com>
2024-12-13 19:38:34 +0500

    Добавлен базовый класс, чтобы можно в дальнейшем быть ленивым!

---
+++ ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnWPF.ViewModels.Base
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnProptrtyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value))
+                return false;
+
+            field = value;
+            OnProptrtyChanged(propertyName);
+            return true;
+        }
+    }
+}
