3c2ddd5110cd1fe7ccf9b0bb840ba4d287fb50f8
ChristianRekfild <ChrisRekfild@gmail.com>
2024-12-14 22:43:28 +0500

    Создан главный класс Команды

---
+++ Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,25 @@
+﻿using LearnWPF.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnWPF.Infrastructure.Commands
+{
+    class LambdaCommand : CommandBase
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public LambdaCommand(Action<Object> execute, Func<object, bool> canExecute = null)
+        {
+            this._execute = execute ?? throw new ArgumentException($"Не передан параметр {nameof(execute)}");
+            this._canExecute = canExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _execute(parameter);
+    }
+}
